bfbfcc0c40cc275dc64602d6d7560b1780112601
Alisa <Workbabko@yandex.ru>
2021-09-27 23:06:24 +0300

    Внес правки в дз 1 раз

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 //Функция для получения рандомного числа в заданных диапазонах
 
-let getRandomNum = function (min, max) {
+const getRandom = function (min, max) {
   if (max > min && min >= 0 && max >= 0) {
     max -= min;
     return parseInt((Math.random() * ++max) + min);
   } else if ((max <= min && min >= 0 && max >= 0)) {
-    let change = max;
+    const change = max;
     max = min;
     min = change;
     max -= min;
     return parseInt((Math.random() * ++max) + min);
   }
-  return console.log('Вы указали некорректные аргументы');
 };
+getRandom(1,2);
 
 //Функция для проверки максимальной длины строки
 
-let checkFieldLength = function (string, maxLength) {
-  if (maxLength >= string.length) {
-    return true;
-  }
-  return false;
+const checkFieldLength = function (string, maxLength) {
+  return maxLength >= string.length;
 };
+checkFieldLength('fd',3);
