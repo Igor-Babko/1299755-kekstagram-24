e79ed16fe6b21268c63bb9984d6f64e7752dd246
Alisa <Workbabko@yandex.ru>
2021-10-04 23:42:48 +0300

    Внес первые правки к заданию по генерации 25 объектов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Функция для получения рандомного числа в заданных диапазонах
 
-const getRandom = function (min, max) {
+const getRandom = (min, max) => {
   if (max > min && min >= 0 && max >= 0) {
     max -= min;
     return Math.floor((Math.random() * ++max) + min);
@@ -23,45 +23,43 @@ const getRandom = function (min, max) {
 
 //Функция для генерации объектов
 
-const getComments = function (quantity) {
+const USER_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const USER_NAMES = ['Катя', 'Оля', 'Дима', 'Игорь', 'Наташа', 'Вася', 'Оксана', 'Тимур'];
+const generatedIds = [];
 
-  let object;
-  const massive = [];
+//Функция для генерации уникального id комментария
 
-  function createComments() {
-    const idMassive = [];
-    let uniqId;
-    let quantityComments;
-    let newId;
-    const commentsMassive = [];
-    const userComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']
-    const userNames = ['Катя', 'Оля', 'Дима', 'Игорь', 'Наташа', 'Вася', 'Оксана', 'Тимур'];
-
-    quantityComments = Math.floor(Math.random() * 10 + 1); //Генерируем случайное количество комментариев (1-10)
-    for (let j = 0; j < quantityComments; j++) { //Генерируем эти комментарии
-      newId = Math.floor(Math.random() * 1000 + 1);
-      //Генерируем случайный ID от 1 до 1000
-      uniqId = true; //По умолчанию ставим наш id уникальным
-      for (let i = 0; i < quantityComments; i++) { //Проверяем наш id на уникальность
-        if (idMassive[i] === newId) {
-          uniqId = false;
-        }
-      }
-      if (uniqId === false) { //Если id не уникальный, возвращаемся на этап генерации комментария
-        j--;
-        continue;
-      }
-
-      idMassive.push(newId);
-      commentsMassive[j] = {
-        id: newId,
-        avatar: `img/avatar-${  Math.floor(Math.random() * 6 + 1)  }.svg`,
-        message: `${userComments[Math.floor(Math.random() * 7)]  } ${  userComments[Math.floor(Math.random() * 7)]}`,
-        name: userNames[Math.floor(Math.random() * 8)],
-      };
-    }
-    return commentsMassive;
+const generateCommentId = () => {
+  let id = getRandom(1, 1000);
+  while (generatedIds.includes(id)) {
+    id = getRandom(1, 1000);
   }
+  return id;
+};
+
+//Функция для генерации комментария
+
+const createComments = () => {
+  const commentsMassive = [];
+  const quantityComments = Math.floor(Math.random() * 10 + 1); //Генерируем случайное количество комментариев (1-10)
+  for (let j = 0; j < quantityComments; j++) { //Генерируем эти комментарии
+    const id = generateCommentId();
+    generatedIds.push(id);
+    commentsMassive[j] = {
+      id: id,
+      avatar: `img/avatar-${  Math.floor(Math.random() * 6 + 1)  }.svg`,
+      message: `${USER_COMMENTS[Math.floor(Math.random() * 7)]  } ${  USER_COMMENTS[Math.floor(Math.random() * 7)]}`,
+      name: USER_NAMES[Math.floor(Math.random() * 8)],
+    };
+  }
+  return commentsMassive;
+};
+
+//Функция для генерации поста
+
+const getPosts = (quantity) => {
+  const massive = [];
+  let object;
   for (let i = 0; i < quantity; i++) {
     object = {
       id: i + 1,
@@ -74,4 +72,4 @@ const getComments = function (quantity) {
   }
   return massive;
 };
-getComments(25);
+getPosts(25);
