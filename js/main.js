6fcf19d6a281086d8453939d3a3a108049248b10
Alisa <Workbabko@yandex.ru>
2021-10-26 16:47:27 +0300

    Внес правки с camelCase

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {createPosts} from './utils/data.js';
-import {renderPosts} from './utils/RenderingThumbnails.js';
+import {renderPosts} from './utils/renderingThumbnails.js';
 
 const data = createPosts(25);
 renderPosts(25);
