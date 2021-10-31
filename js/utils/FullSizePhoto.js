106c303f80a0a03cbb8fa47fa62699e2a7eefd46
Alisa <Workbabko@yandex.ru>
2021-10-31 16:53:57 +0300

    пофиксил баг с объявлением функции в функции

--- js/utils/FullSizePhoto.js
+++ js/utils/FullSizePhoto.js
@@ -7,69 +7,72 @@ const bigPicture = document.querySelector('.big-picture');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const pictureCancel = document.querySelector('#picture-cancel');
 const commentsLoader = document.querySelector('.comments-loader');
-const fieldSet = document.querySelector('fieldset');
 
 pictures.addEventListener('click', (evt) => {
 
-  if(evt.target !== pictures && evt.target !== fieldSet) {
-  const pictureElement = evt.target.closest('.picture');
-  const arrayPictures = Array.from(document.querySelectorAll('.picture'));
-  const index = arrayPictures.indexOf(pictureElement);
-  const data = generatedPosts[index];
 
-  const socialComments = document.querySelector('.social__comments');
-  const documentFragment = document.createDocumentFragment();
-
-  socialComments.innerHTML = '';
-
-  document.body.classList.add('modal-open');
-  bigPicture.classList.remove('hidden');
-
-  bigPicture.querySelector('.big-picture__img').querySelector('img').src = data.url;
-  bigPicture.querySelector('.likes-count').textContent = data.likes;
-  bigPicture.querySelector('.comments-count').textContent = data.comments.length;
-
-  data.comments.forEach(({ message, name, avatar}) => {
-
-    const comment = document.createElement('li');
-    comment.classList.add('social__comment');
-
-    const img = document.createElement('img');
-    img.classList.add('social__picture');
-    img.src = avatar;
-    img.alt = name;
-
-    const p = document.createElement('p');
-    comment.classList.add('social__text');
-    p.textContent = message;
-
-    comment.appendChild(img);
-    comment.appendChild(p);
-
-    documentFragment.appendChild(comment);
-  });
-
-  socialComments.appendChild(documentFragment);
-  bigPicture.querySelector('.social__caption').textContent = data.description;
-
-
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
-  document.body.classList.add('modal-open');
-  pictureCancel.addEventListener('click', closeBigPhoto);
-  document.addEventListener('keydown', onPopupEscKeydown);
-
-  function onPopupEscKeydown(event){
+  function onPopupEscKeydown(event) {
     if (event.key === 'Escape') {
       event.preventDefault();
       closeBigPhoto();
     }
-  };
+  }
 
   function closeBigPhoto() {
     bigPicture.classList.add('hidden');
     document.body.classList.remove('modal-open');
     document.removeEventListener('keydown', onPopupEscKeydown);
     pictureCancel.removeEventListener('click', closeBigPhoto);
-  };
-}});
+  }
+
+  if (evt.target.matches('img')) {
+    const pictureElement = evt.target.closest('.picture');
+    const arrayPictures = Array.from(document.querySelectorAll('.picture'));
+    const index = arrayPictures.indexOf(pictureElement);
+    const data = generatedPosts[index];
+
+    const socialComments = document.querySelector('.social__comments');
+    const documentFragment = document.createDocumentFragment();
+
+    socialComments.innerHTML = '';
+
+    document.body.classList.add('modal-open');
+    bigPicture.classList.remove('hidden');
+
+    bigPicture.querySelector('.big-picture__img').querySelector('img').src = data.url;
+    bigPicture.querySelector('.likes-count').textContent = data.likes;
+    bigPicture.querySelector('.comments-count').textContent = data.comments.length;
+
+    data.comments.forEach(({message,name,avatar}) => {
+
+      const comment = document.createElement('li');
+      comment.classList.add('social__comment');
+
+      const img = document.createElement('img');
+      img.classList.add('social__picture');
+      img.src = avatar;
+      img.alt = name;
+
+      const p = document.createElement('p');
+      comment.classList.add('social__text');
+      p.textContent = message;
+
+      comment.appendChild(img);
+      comment.appendChild(p);
+
+      documentFragment.appendChild(comment);
+    });
+
+    socialComments.appendChild(documentFragment);
+    bigPicture.querySelector('.social__caption').textContent = data.description;
+
+
+    socialCommentCount.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+    document.body.classList.add('modal-open');
+    pictureCancel.addEventListener('click', closeBigPhoto);
+    document.addEventListener('keydown', onPopupEscKeydown);
+
+
+  }
+});
