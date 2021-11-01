36168563d3ef4059fffd8cf7e4d603d83e5f33c0
Alisa <Workbabko@yandex.ru>
2021-11-01 22:57:59 +0300

    Рабочий код до добавления Хэштегов

--- js/utils/form.js
+++ js/utils/form.js
@@ -1,3 +1,33 @@
-const form = document.querySelector('form');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const uploadFile = document.querySelector('#upload-file');
+const body = document.querySelector('body');
+const uploadCancelButton = document.querySelector('#upload-cancel');
+const scaleControlValue = document.querySelector('.scale__control--value');
 
-form.action = 'https://24.javascript.pages.academy/kekstagram';
+
+uploadFile.addEventListener('change', ()=>{
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onEscKeydown);
+  uploadCancelButton.addEventListener('click', closeForm);
+
+
+});
+
+function onEscKeydown(event) {
+  if (event.key === 'Escape') {
+    event.preventDefault();
+    closeForm();
+  }
+}
+
+function closeForm() {
+  imgUploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeydown);
+  uploadCancelButton.removeEventListener('click', closeForm);
+  uploadFile.value ='';
+  scaleControlValue.value = '55%';
+}
+
